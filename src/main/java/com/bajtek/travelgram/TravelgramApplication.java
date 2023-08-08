f8d0e21b876fdf0d3c81a610c70733cc1a64a107
Bartłomiej Laskowski <bartekl505@wp.pl>
2023-08-08 15:45:37 +0200

    Initial commit

---
+++ src/main/java/com/bajtek/travelgram/TravelgramApplication.java
@@ -0,0 +1,13 @@
+package com.bajtek.travelgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bajtek/travelgram/TravelgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajtek.travelgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
